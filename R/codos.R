5942c86e2f7024f56726c785cb0a2674d345cbaa
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-11-10 11:57:28 +0000

    Port utils from Python

--- R/codos.R
+++ R/codos.R
@@ -64,7 +64,7 @@ e_difference <- function(self, m_true) {
 useable_e <- function(self, T, m, pre_K, pre_eta, pre_sec_E_q) {
   # We pre-calculate E_q so we don't have to calculate it twice in the different eqm functions
   pre_E_q <- pre_calc_E_q(T,m, pre_sec_E_q)
-  cur_eqm <- abs(eqm(pre_E_q, m))
+  cur_eqm <- abs(eqm(self, pre_E_q, m))
   cur_eqm * (self$C * sqrt(pre_eta/pre_K) * cur_eqm ^ (1 / self$D_root_factor) + 1) ^ -1
 }
 
@@ -76,4 +76,85 @@ get_c_i <- function(self) {
 # Determines if the compensation point 'law' is upheld
 compensation_point_held <- function(self, m) {
   c_i(self$T_rec, self$c_ratio * self$modern_CO2, m) > true_compensation_point(self$T_rec)
+}
+
+pow <- function(x, y) {
+  x ^ y
+}
+
+## Static methods for the P_model_inverter class ##################
+eqm <- function(self, pre_E_q, m) {
+  # This abs is extremely suspect however this should be fine when m>0
+  pre_E_q * (pow(abs(1 + pow(m, self$omega)), (1 / self$omega)) - m )
+}
+
+# We can pre-calculate a lot of the E_q hence we wont use this function much
+E_q <- function(self, T, m) {
+  R_n(self, T, m) / self$lam * pow(1 + self$gamma * pow(self$c + T, 2) / (self$abc) * exp(-self$b * T / (self$c + T)), -1)
+}
+
+pre_calc_E_q <- function(self, T, m, pre) {
+  R_n(self, T, m) * pre
+}
+
+pre_section_E_q <- function(self, T) {
+  pow(1 + self$gamma * pow(self$c + T, 2) / self$abc * exp(-self$b *T / (self$c + T)), -1) / self$lam
+}
+
+# Gives the value for R_n in MJ kg^(-1) a^(-1) mm
+R_n <- function(self, T, m, lat = -30 ) {
+  scale_factor <- 365.24 * 24 * 60 * 60 * 10 ^ -6
+  scale_factor * (0.83 * self$R_o * (0.25 + 0.5 * S_f(m)) - (107 - T) * (0.2 + 0.8 * S_f(m)))
+}
+
+# Fraction of sunshine hours
+S_f <- function(m) {
+  0.6611 * exp(-0.74 * m) + 0.2175
+}
+
+K <- function(self, T) {
+  # Don't have a better name for this
+  pre_calc <- 1 /self$R * (1 / 298 - 1 / (T + 273.15))
+  404.9 * exp(self$dHc * pre_calc) * ( 1 + self$O / (278.4 * exp(self$dHo * pre_calc)))
+}
+
+eta <- function(self, T) {
+  0.024258 * exp(580 / (T + self$visc_offset))
+}
+
+# Calculates c_i, could be sped up
+c_i <- function(self, T, c_a, m) {
+  c_a / c_a_c_i_ratio(self, T, m)
+}
+
+c_a_c_i_ratio <- function(self, T, m) {
+  delta_e <- eqm(self, E_q(self, T, m), m)
+  1 + self$C * sqrt(eta(self, T) / K(self, T)) * delta_e ^ (1/self$D_root_factor)
+}
+
+compensation_point <- function(self, T) {
+  42.75 * exp((self$delta_H / self$R) * (1 / 298 - 1 / (273.15 + T)))
+}
+
+true_compensation_point <- function(self, T) {
+  compensation_point(self, T) + self$dark_scale * K(T)
+}
+
+# Budyko relationship to give alpha from mi
+alpha_from_mi_om3 <- function(mi) {
+  1 + mi - (1 + mi ^ 3) ^ (1/3)
+}
+
+## Wrapper functions to find corrected moisture index #############
+calculate_m_true <- function(T_diff, T_ref, m_rec, c_a_diff) {
+  model <- p_model_inverter(T_diff, T_ref, m_rec, c_a_diff)
+
+  delta_m <- solve_for_delta_m(self)
+  comp_point_held <- compensation_point_held(self, m_rec)
+
+  output <- m_rec
+  if (comp_point_held)
+    output <- output + delta_m
+
+  c(output, comp_point_held, get_c_i(model))
 }
