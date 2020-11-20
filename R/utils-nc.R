7c2a1ad40a7d69881081b8a5c6f83f98c4b71df0
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-11-20 17:04:41 +0000

    Add convert_units function

--- R/utils-nc.R
+++ R/utils-nc.R
@@ -1,3 +1,146 @@
+#' Convert units from a netCDF file
+#'
+#' @param new_units String with the new units.
+#' @param conv_factor Numeric vector or single value with the conversion factor.
+#' @param FUN Infix function to perform the conversion.
+#'
+#' @inheritParams nc2ts
+#'
+#' @export
+convert_units <- function(filename,
+                          varid,
+                          new_units,
+                          conv_factor,
+                          timeid = "time",
+                          latid = "lat",
+                          lonid = "lon",
+                          FUN = `*`) {
+  if (!file.exists(filename))
+    stop("The given netCDF file was not found: \n", filename, call. = FALSE)
+  nc <- ncdf4::nc_open(filename)
+  on.exit(ncdf4::nc_close(nc)) # Close the file
+  # Check the dimensions for time, latitude, and longitude exist
+  idx <- c(timeid, latid, lonid) %in% names(nc$dim)
+  if (any(!idx))
+    stop("The following dimension",
+         ifelse(sum(!idx) > 1, "s were", " was"),
+         " not found: \n",
+         paste0("- ", c(timeid, latid, lonid)[!idx], collapse = "\n"),
+         call. = FALSE)
+
+  # Check the main variable exists
+  if (!(varid %in% names(nc$var)))
+    stop("The main variable was not found: \n- ", varid,
+         "\nTry one of the following: \n",
+         paste0("- ", names(nc$var), collapse = "\n"),
+         call. = FALSE)
+
+  # Read dimensions
+  ## Time
+  tryCatch({
+    # time_data <- tibble::as_tibble(ncdf4::ncvar_get(nc, timeid))
+    time_data <- ncdf4::ncvar_get(nc, timeid)
+    time_units <- ncdf4::ncatt_get(nc, timeid, "units")$value
+  }, error = function(e) {
+    stop("Error reading the time dimension: ", timeid, call. = FALSE)
+  })
+  ## Latitude
+  tryCatch({
+    # lat_data <- tibble::as_tibble(ncdf4::ncvar_get(nc, latid))
+    lat_data <- ncdf4::ncvar_get(nc, latid)
+    lat_units <- ncdf4::ncatt_get(nc, latid, "units")$value
+  }, error = function(e) {
+    stop("Error reading the latitude dimension: ", latid, call. = FALSE)
+  })
+  ## Longitude
+  tryCatch({
+    # lon_data <- tibble::as_tibble(ncdf4::ncvar_get(nc, lonid))
+    lon_data <- ncdf4::ncvar_get(nc, lonid)
+    lon_units <- ncdf4::ncatt_get(nc, lonid, "units")$value
+  }, error = function(e) {
+    stop("Error reading the longitude dimension: ", lonid, call. = FALSE)
+  })
+
+  # Read main variable
+  tryCatch({
+    var_data <- ncdf4::ncvar_get(nc, varid)
+    var_units <- ncdf4::ncatt_get(nc, varid, "units")$value
+  }, error = function(e) {
+    stop("Error reading the main variable: ", varid, call. = FALSE)
+  })
+
+  # Create empty structure with the same dimensions as var_data
+  var_data2 <- array(0, dim = dim(var_data))
+
+  if (length(conv_factor) != length(time_data) & length(conv_factor) > 1)
+    stop("The conversion factor must be of the same length of the time ",
+         "dimension or a single value.", call. = FALSE)
+
+  # Verify that conversion factor's length is the same as the time dimension
+  if (length(conv_factor) != length(time_data)) {
+    conv_factor <- rep(conv_factor, length(time_data))
+  }
+
+  # Convert units
+  for (i in seq_along(time_data)) {
+    var_data2[,,i] <- FUN(var_data[,,i], conv_factor[i])
+  }
+
+  # Create name for the output file based on input netCDF
+  output_filename <- paste0(gsub("\\.nc$", "", filename), "-new.nc")
+
+  # Check if the output file exists
+  if (file.exists(output_filename) & !overwrite)
+    stop("The output netCDF already exists. Please rename it or pass ",
+         "overwrite = TRUE to the function call.\n",
+         output_filename, call. = FALSE)
+
+  # Delete old output file
+  if (file.exists(output_filename))
+    . <- file.remove(output_filename)
+
+  # Extract extra attributes from the input netCDF
+  time_calendar <- ncdf4::ncatt_get(nc, timeid, "calendar")$value
+  var_longname <- ncdf4::ncatt_get(nc, varid, "long_name")$value
+  var_missval <- ncdf4::ncatt_get(nc, varid, "missing_value")$value
+
+  # Define dimensions
+  dimLat <- ncdf4::ncdim_def(name = latid, units = lat_units, vals = lat_data)
+  dimLon <- ncdf4::ncdim_def(name = lonid, units = lon_units, vals = lon_data)
+  dimTime <- ncdf4::ncdim_def(name = timeid,
+                              units = time_units,
+                              vals = time_data,
+                              calendar = time_calendar)
+
+  dimLon$id <- 0
+  dimLat$id <- 1
+  dimTime$id <- 2
+
+  # Create a variable
+  var_conv <- ncdf4::ncvar_def(name = varid,
+                               units = new_units,
+                               dim = list(dimLon, dimLat, dimTime),
+                               missval = var_missval,
+                               prec = "double",
+                               longname = var_longname)
+
+  # Create new netCDF file
+  nc_out <- ncdf4::nc_create(output_filename, var_conv)
+  on.exit(ncdf4::nc_close(nc_out)) # Close the file
+
+  # List all attributes for the main variable in the input netCDF
+  var_att <- ncdf4::ncatt_get(nc, varid)
+  var_att_names <- names(var_att)
+  idx <- !(var_att_names %in% c("long_name", "units", "_FillValue"))
+  # Add extra attributes to the new netCDF
+  for (i in which(idx))
+    ncdf4::ncatt_put(nc_out, varid, var_att_names[i], var_att[[i]])
+
+  # Add the climatology data
+  ncdf4::ncvar_put(nc_out, var_conv, var_data2)
+  return(var_data2)
+}
+
 #' Get the days in a month
 #'
 #' Get the days in a month from a date string.
