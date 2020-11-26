0aa33464778a16c4cf9cbe1cf90fa85e5dfc6986
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-11-26 22:23:31 +0000

    Minor changes

--- R.ignore/utils-plots.R
+++ R.ignore/utils-plots.R
@@ -30,6 +30,7 @@ vap_ts_int <- codos::extract_data(ncfiles_int[5], "vap")$main$data
 lat <- codos::extract_data(ncfiles_clim[1], "cld")$lat$data
 lon <- codos::extract_data(ncfiles_clim[1], "cld")$lon$data
 
+# Test
 interpolated <- c(cld_ts_int[650, 120, ],
                   pre_ts_int[650, 120, ],
                   tmn_ts_int[650, 120, ],
@@ -40,12 +41,13 @@ climatology <- c(cld_ts_clim[650, 120, ],
                  tmn_ts_clim[650, 120, ],
                  tmx_ts_clim[650, 120, ],
                  vap_ts_clim[650, 120, ])
-month_len <- codos::days_in_month(paste0("1961-", out$time$data, "-01"))
-ts_comp(climatology,
+month_len <- codos::days_in_month(paste0("1961-", 1:12, "-01"))
+codos::ts_comp(climatology,
         interpolated,
         month_len,
         main = paste0("Time series at (", lat[j], ", ", lon[i], ")"),
         xlab = "Days")
+
 
 lon_start <- 650
 lon_end <- 655
@@ -84,26 +86,26 @@ ggplot2::ggsave("ts-comparison.pdf",
                 path = "~/Desktop/iCloud/UoR/Data/codos",
                 limitsize = FALSE)
 
-ts_comp <- function(climatology,
-                    interpolated,
-                    month_len,
-                    vars = c("cld", "pre", "tmn", "tmx", "vap"),
-                    days = 365,
-                    months = 12,
-                    main = NULL,
-                    xlab = NULL,
-                    ylab = NULL) {
-  df <- data.frame(x = rep(seq_len(days), length(vars)),
-                   y = interpolated,
-                   variable = rep(vars, each = days))
-  df2 <- data.frame(x = cumsum(month_len) - month_len / 2,
-                    y = climatology,
-                    variable = rep(vars, each = months))
-  ggplot2::ggplot(df, ggplot2::aes(x, y)) +
-    ggplot2::geom_line(ggplot2::aes(color = variable, linetype = variable)) +
-    ggplot2::geom_point(ggplot2::aes(x, y, color = variable), df2) +
-    ggplot2::labs(title = main, x = xlab, y = ylab) +
-    ggplot2::scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
-    ggplot2::scale_color_brewer(palette = "Set1") +
-    ggplot2::theme_bw()
-}
+# ts_comp <- function(climatology,
+#                     interpolated,
+#                     month_len,
+#                     vars = c("cld", "pre", "tmn", "tmx", "vap"),
+#                     days = 365,
+#                     months = 12,
+#                     main = NULL,
+#                     xlab = NULL,
+#                     ylab = NULL) {
+#   df <- data.frame(x = rep(seq_len(days), length(vars)),
+#                    y = interpolated,
+#                    variable = rep(vars, each = days))
+#   df2 <- data.frame(x = cumsum(month_len) - month_len / 2,
+#                     y = climatology,
+#                     variable = rep(vars, each = months))
+#   ggplot2::ggplot(df, ggplot2::aes(x, y)) +
+#     ggplot2::geom_line(ggplot2::aes(color = variable, linetype = variable)) +
+#     ggplot2::geom_point(ggplot2::aes(x, y, color = variable), df2) +
+#     ggplot2::labs(title = main, x = xlab, y = ylab) +
+#     ggplot2::scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
+#     ggplot2::scale_color_brewer(palette = "Set1") +
+#     ggplot2::theme_bw()
+# }
