71e47798091a0039111ac1b78aa931c33417e524
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-11-26 09:28:18 +0000

    Add documentation for datasets, lat and lon

---
+++ R/data.R
@@ -0,0 +1,33 @@
+#' Latitude dataset
+#'
+#' A dataset containing information of the latitude dimension at a resolution
+#' of 0.5 deg.
+#'
+#' @format A list with 4 elements:
+#' \describe{
+#'     \item{data}{Numeric vector with the latitude values.}
+#'     \item{id}{Identifier to use in netCDF files.}
+#'     \item{longname}{String with long name of the variable.}
+#'     \item{units}{Latitude units.}
+#' }
+#'
+#' @usage data(lat)
+#' @keywords datasets
+"lat"
+
+#' Longitude dataset
+#'
+#' A dataset containing information of the longitude dimension at a resolution
+#' of 0.5 deg.
+#'
+#' @format A list with 4 elements:
+#' \describe{
+#'     \item{data}{Numeric vector with the longitude values.}
+#'     \item{id}{Identifier to use in netCDF files.}
+#'     \item{longname}{String with long name of the variable.}
+#'     \item{units}{Longitude units.}
+#' }
+#'
+#' @usage data(lon)
+#' @keywords datasets
+"lon"
