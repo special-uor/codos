fee15e09e716297af17aad8fd72f59f91a6b17f9
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-01-13 12:57:57 +0000

    Make lat and lon data sets internal

--- data-raw/lon.R
+++ data-raw/lon.R
@@ -4,4 +4,4 @@ lon <- list(data = seq(-179.75, 179.75, 0.5),
             longname = "longitude",
             units = "degrees_east")
 
-usethis::use_data(lon, overwrite = TRUE)
+usethis::use_data(lon, overwrite = TRUE, internal = TRUE)
