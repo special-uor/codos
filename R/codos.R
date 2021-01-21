8ecd4a6e2fbf4c7c1b310d69e5df00e2e33e4c4c
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-01-21 17:34:05 +0000

    Add past_co2 function

--- R/codos.R
+++ R/codos.R
@@ -159,6 +159,103 @@ f <- function(Tc, MI, co2, scale_factor = 101.325 * 10^-3) {
   # (E(Tc) * sqrt(vpd_internal(Tc, MI)) + vpd_internal(Tc, MI)) / (co2 - compensation_point(Tc))
 }
 
+
+#' Find past CO2
+#'
+#' Find past CO2 linked to a given \code{age}.
+#'
+#' @param age Numeric value with the \code{age}.
+#' @param ref Reference data frame containing ice core composite
+#'     information, defaults to \code{\link{codos::ice_core}}:
+#'
+#' Bereiter, B., Eggleston, S., Schmitt, J., Nehrbass‐Ahles, C., Stocker, T. F.,
+#' Fischer, H., Kipfstuhl, S., and Chappellaz, J. (2015), Revision of the EPICA
+#' Dome C CO2 record from 800 to 600 kyr before present, Geophys. Res. Lett.,
+#' 42, 542– 549, <doi:10.1002/2014GL061957>.
+#' @param digits Number of significant digits used to match \code{age} to
+#'     the age values in \code{ref}.
+#' @param use_loess Boolean flag to indicate whether to use a LOESS smoothing
+#'     (\code{TRUE}) or use adjacent records to find the corresponding value of
+#'     CO2 (\code{FALSE}).
+#' @param ... Extra arguments passed to \code{\link[stats:loess]{stats::loess}}.
+#'
+#' @return Numeric value of CO2 linked to the given \code{age}.
+#' @export
+#'
+#' @examples
+#' codos::past_co2(-47)
+#' codos::past_co2(-47, use_loess = TRUE)
+#' codos::past_co2(-47, use_loess = TRUE, span = 0.05)
+past_co2 <- function(age,
+                     ref = codos::ice_core,
+                     digits = 2,
+                     use_loess = FALSE,
+                     ...) {
+  if (use_loess)
+    return(past_co2_loess(age = age,
+                          ref = ref,
+                          digits = digits,
+                          ...))
+
+  # Check the reference tibble, must have at least two columns
+  if (ncol(ref) < 2)
+    stop("The `ref` data frame, must have at least two columns: ",
+         "`age` and `co2`.", call. = FALSE)
+  # Extract the reference age and co2
+  ref_age <- purrr::pluck(ref, 1)
+  ref_co2 <- purrr::pluck(ref, 2)
+  # Check for exact match, rounding to `digits`
+  if (any(round(ref_age, digits) == age))
+    return(ref_co2[round(ref_age, digits) == age])
+  # Sort by age, ascending
+  idx <- order(ref_age)
+  ref_age <- ref_age[idx]
+  ref_co2 <- ref_co2[idx]
+  # Find the nearest ref_age to the given age
+  idx <- sort(order(abs(ref_age - age))[1:2])
+  down <- idx[1]
+  up <- idx[2]
+  if ((abs(age) > abs(ref_age[down]) & age < 0) |
+      (abs(age) < abs(ref_age[down]) & age > 0))
+    return(ref_co2[down])
+  if ((abs(age) < abs(ref_age[up]) & age < 0) |
+      (abs(age) > abs(ref_age[up]) & age > 0))
+    return(ref_co2[up])
+  return(mean(ref_co2[c(down, up)]))
+}
+
+#' Find past CO2 using \code{loess}
+#'
+#' Find past CO2 linked to a given \code{age} using \code{loess}.
+#'
+#' @param age Numeric value with the \code{age}.
+#' @param ref Reference data frame containing ice core composite
+#'     information, defaults to \code{\link{codos::ice_core}}:
+#'
+#' Bereiter, B., Eggleston, S., Schmitt, J., Nehrbass‐Ahles, C., Stocker, T. F.,
+#' Fischer, H., Kipfstuhl, S., and Chappellaz, J. (2015), Revision of the EPICA
+#' Dome C CO2 record from 800 to 600 kyr before present, Geophys. Res. Lett.,
+#' 42, 542– 549, <doi:10.1002/2014GL061957>.
+#' @param span Numeric value, \eqn{\alpha}, which controls the degree of
+#' smoothing.
+#' @param ... Extra arguments passed to \code{\link[stats:loess]{stats::loess}}.
+#'
+#' @return Numeric value of CO2 linked to the given \code{age}.
+#' @keywords internal
+past_co2_loess <- function(age, ref = codos::ice_core, span = 0.1, ...) {
+  # Check the reference tibble, must have at least two columns
+  if (ncol(ref) < 2)
+    stop("The `ref` data frame, must have at least two columns: ",
+         "`age` and `co2`.", call. = FALSE)
+  # Extract the reference age and co2
+  ref_age <- purrr::pluck(ref, 1)
+  ref_co2 <- purrr::pluck(ref, 2)
+  loessMod10 <- loess(co2 ~ age,
+                      tibble::tibble(age = ref_age,
+                                     co2 = ref_co2), span = span, ...)
+  return(predict(loessMod10, age))
+}
+
 #' Calculate corrected moisture index (MI)
 #'
 #' Calculate corrected moisture index (MI) based on reconstructed MI, CO2 and
